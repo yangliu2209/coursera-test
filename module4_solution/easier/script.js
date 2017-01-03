042e61fedd0df422d319aa77f49d0a6e87af3814
yangliu2209 <yangparisliu@gmail.com>
2017-01-03 15:43:59 -0500

    Update script.js

--- module4_solution/easier/script.js
+++ module4_solution/easier/script.js
@@ -1,1 +1,13 @@
-
+(function (){
+  
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+  for(var i=0; i<names.length; i++){
+    var firstLetter = names[i].CharAt(0).toLowerCase();
+    if (firstLetter === 'j'){
+      byeSpeaker.speak(name[i]);
+    } else {
+      helloSpeaker.speak(name[i]);
+    }
+  }
+});()
