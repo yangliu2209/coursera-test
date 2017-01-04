d37624b939155988f9c2c90f373c7e67026fc405
yangliu2209 <yangparisliu@gmail.com>
2017-01-04 15:33:10 -0500

    Update script.js

--- module4_solution/easier/script.js
+++ module4_solution/easier/script.js
@@ -1,13 +1,15 @@
 (function (){
-  
+  //create a array of list of names;
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  
+  // loop for listing name
   for(var i=0; i<names.length; i++){
+    //create a var firstletter to find the first letter of each name in the array
     var firstLetter = names[i].CharAt(0).toLowerCase();
+    //if...else.. to meet the requirement
     if (firstLetter === 'j'){
       byeSpeaker.speak(names[i]);
     } else {
       helloSpeaker.speak(names[i]);
     }
   }
-});()
+}) ();
